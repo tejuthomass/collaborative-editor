cf21f4a58fe4d28bb5e39e9103ecf498017abba5
Teju Thomas <thomasteju7@gmail.com>
2024-07-31 17:06:03 +0530

    Implement document editor page

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Header = () => {
+const Header = ({ children }: HeaderProps) => {
   return (
     <div className='header'>
         <Link href='/' className='md:flex-1'>
             <Image 
-                src='/assets/icons/logo.svg'
+                src="/assets/icons/logo.svg"
                 alt='Logo with name'
                 width={120}
                 height={32}
